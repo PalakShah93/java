eff0f1825f873179ba0c1f52e1e1d0bbffb46794
Palak <palakjerry93@gmail.com>
2017-04-23 00:22:20 +0800

    add sum of i and b

--- src/TryInt.java
+++ src/TryInt.java
@@ -5,16 +5,18 @@ public class TryInt {
 	
 	{
 		
-		int i ;
+		int i ; // Minimum value is - 2,147,483,648 (-2^31) Maximum value is 2,147,483,647(inclusive) (2^31 -1)
 		i = 10;
-		byte b= -29; //minimun -128 maximum is 127, value +- will throw error
-		short s = 27000;
-		
+		byte b= -29; //Minimum -128 maximum is 127, value +- will throw error
+		short s = 27000; // Minimum value is -32,768 (-2^15) Maximum value is 32,767
+		long l= 1000000000000000L; // Minimum value is -9,223,372,036,854,775,808(-2^63) Maximum value is 9,223,372,036,854,775,807 (inclusive)(2^63 -1)
+	
 		
 		System.out.println(i);
 		System.out.println(b);
 		System.out.println(s);
-		
+		System.out.print(l);
+		System.out.println(i+b);
 	}
 
 }
