59501c69421a268f3302dc8c59a7e16326635245
palakshah93 <palakjerry93@gmail.com>
2017-04-27 22:12:25 +0800

    added noteasy file

---
+++ src/NotEasy.java
@@ -0,0 +1,13 @@
+
+public class NotEasy {
+
+	
+	public static void main (String args[]){
+		
+		char a = 'P';
+		
+		System.out.println(a);
+		
+		
+	}
+}
--- src/bank.java
+++ src/bank.java
@@ -1,7 +1,7 @@
 
 public class bank {
 
-	public static void main(String[] args) {
+	public static void main(String args[]) {
 
 		int bankbalance = 500;
 		bankbalance= bankbalance+250;
