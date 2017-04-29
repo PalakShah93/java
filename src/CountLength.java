964ca77a3c683d04c5caf758c91c9b02940bcdb6
Palak <palakjerry93@gmail.com>
2017-04-29 21:55:05 +0800

    added countlength and MathMax program

---
+++ src/CountLength.java
@@ -0,0 +1,13 @@
+
+public class CountLength {
+
+	public static void main(String[] args) {
+
+		String canning ="canning";
+		String rabbit ="Rabbit";
+		int countlength =(canning + rabbit).length() * 5;
+		
+		System.out.println(countlength);
+	}
+
+}
---
+++ src/MathMax.java
@@ -0,0 +1,25 @@
+
+public class MathMax {
+	
+	public static void main (String agrs[]){
+		
+		String car = "car";
+		String fish ="fish";
+		int mathmax = Math.max((car +fish).length(), 5);
+		
+		int parseint =2+Integer.parseInt("121");
+		int parseint1= Integer.parseInt("122",5);
+		int parseint2 =Integer.parseInt("121",9) / Integer.parseInt("123",4);
+		
+		System.out.println(mathmax);
+		System.out.println(parseint);
+		System.out.println(parseint1);
+		System.out.println(parseint2);
+		
+		System.out.println(Math.abs("morning".length())+7);
+		
+		
+		
+	}
+
+}
