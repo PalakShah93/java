e498ad011962d77a0b0b72883844b26d7455372b
Palak <palakjerry93@gmail.com>
2017-05-02 17:54:27 +0800

    nested if

--- src/USA.java
+++ src/USA.java
@@ -4,11 +4,25 @@ public class USA {
 	public static void main (String args[]){
 		
 		String  family ="Masi";
-		boolean USA =false;
+		boolean USA =true;
+		boolean car= true;
+		double temperature = 25.0;
 
 		if(USA){
 			
 			System.out.println("I will meet Mahi Vraj " +family+" And Masa family");
+			if(temperature<25){
+				
+				System.out.println(" I can enjoy in shopping centre");
+			}
+			else{
+				
+				System.out.println("I can go at beach");
+			}
+		}
+		else if(car){
+			
+			System.out.println("I will go by car and the temperature is " +temperature);
 		}
 		else {
 			
