af123911a74bdbb17fe638f41e0e17824141b1d4
Palak <palakjerry93@gmail.com>
2017-05-22 17:11:20 +0800

    try forloop

---
+++ src/loopOne.java
@@ -0,0 +1,40 @@
+
+public class loopOne {
+
+	public static void main(String[] args) {
+		
+		 for(int i=0;i<=10;i++){   //initialization, declaration, inc /dec
+		    System.out.println(i);  
+		    }  
+		 
+		 
+	 
+		 for (int x=0;x<5;x++){
+			 
+			 System.out.println("*");
+			 
+			 for (int w=10;w<=50;w++){
+				 
+				 System.out.println("^");
+			 }
+		 }
+		 
+		 for (int a=10;a<=50;a++){
+			 
+			 System.out.println("#");
+		 }
+		 
+		 /*
+		 for (int i=0; i<6; i++)
+		  {
+		  for (int j=0; j<i; j++)
+		  {
+		  System.out.print("*");
+		  }
+		  System.out.println("");
+		  }
+		  
+		  */
+	}
+
+}
