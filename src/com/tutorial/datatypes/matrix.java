7c1562082f78f9043caaad23e8ea10ef7fa8b098
Palak <palakjerry93@gmail.com>
2017-05-24 17:31:22 +0800

    Matrix

---
+++ src/com/tutorial/datatypes/matrix.java
@@ -0,0 +1,24 @@
+package com.tutorial.datatypes;
+
+public class matrix {
+
+	public static void main(String[] args) {
+		
+		int no=0;
+	
+		for(int i=1; i<=3;i++){
+			
+			for (int x=1; x<=i;x++)
+			{
+			
+				System.out.println(i);
+
+			}
+		}
+		
+	}
+	
+			
+	}
+
+
