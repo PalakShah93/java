375a9cf9f43ceacf17623485888a7582c540dbeb
Palak <palakjerry93@gmail.com>
2017-05-30 17:39:45 +0800

    may wishes

--- src/com/tutorial/datatypes/matrix.java
+++ src/com/tutorial/datatypes/matrix.java
@@ -2,23 +2,26 @@ package com.tutorial.datatypes;
 
 public class matrix {
 
-	public static void main(String[] args) {
 		
-		int no=0;
-	
-		for(int i=1; i<=3;i++){
-			
-			for (int x=1; x<=i;x++)
-			{
-			
-				System.out.println(i);
-
-			}
-		}
-		
-	}
-	
-			
-	}
+		public static void main(String[] args) {
+			 		
+			 int no=0;
+			 	
+			 		for(int i=1; i<=3;i++){
+			 			
+			 			for (int x<=i; x<=3;x++)
+			 			{
+			 			
+			 				System.out.print(no+1);
+			 
+			 			}
+			 			System.out.println("");
+			 		}
+			 		
+			 	}
+			 	
+			 			
+			 	}
+			 
 
 
---
+++ src/may.java
@@ -0,0 +1,37 @@
+
+public class may {
+	
+	void godbless(){
+		
+		System.out.println("May God Bless you!!HaPpY BiRthDay...HaPpY BiRthDay.. ");
+	}
+	
+	void experience(){
+		
+		System.out.println("May this year be the best among the rest...");
+	//	System.out.println("\n");
+	}
+	
+	void birthday(){
+		
+		System.out.println("HAVE A HAPPY HAPPY BIRTHDAY");
+		
+	}
+
+	public static void main(String[] args) {
+
+		String special1="HaPpY BiRthDay";
+	    String special2="To You!!!!";
+	    String itsfunday=special1 + " " + special2;
+	    
+	    System.out.println(itsfunday);
+	    
+	    may obj=new may();
+	    obj.godbless();
+	    obj.experience();
+	    obj.birthday();
+	    
+	    
+	}
+
+}
